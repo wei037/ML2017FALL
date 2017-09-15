c516d1bc1f7038a196879cb57f50128ede1daa03
r06922075 <r06922075@ntu.edu.tw>
2017-09-15 17:46:50 +0800

    Upload Q1
    As title

---
+++ hw0/Q1.py
@@ -0,0 +1,28 @@
+with open('words.txt') as f :
+	data = f.read()
+f.close()
+data = data.replace('\n',' ')
+data_list = data.split(' ')[:-1]
+
+word = []
+cnt = []
+for i in range(len(data_list)) :
+	found = 0
+	for j in range(len(word)) :
+		if data_list[i] == word[j] :
+			found = 1
+			cnt[j] += 1
+			break
+	if found == 0 :
+		word.append(data_list[i])
+		cnt.append(1)
+
+out = open('Q1.txt', 'w')
+
+for i in range(len(word)) :
+	str1 = str(word[i]) + ' ' + str(i) + ' ' + str(cnt[i])
+	if i != len(word) - 1 :
+		str1 = str1 + '\n'
+	out.write(str1)	
+
+out.close()
