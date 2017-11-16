f117cb213de3f8a8e57b76c663f4776866a8b711
r06922075_Lab <r06922075@ntu.edu.tw>
2017-11-16 13:45:59 +0800

    add sh

---
+++ hw3/hw3_test.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+wget https://github.com/wei037/ML2017FALL/releases/download/0.0/64753.h5
+wget https://github.com/wei037/ML2017FALL/releases/download/0.0/65700.h5
+python3 ensamble_test.py $1 $2
---
+++ hw3/hw3_train.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+python3 CNN_train.py $1
+ 
