ae984857e2860a7c35ea9b07e01bcb97733faf69
r06922075 <r06922075@ntu.edu.tw>
2017-11-16 04:49:05 -0800

    delete wget

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,5 +1,3 @@
 #! /bin/sh
 
-wget https://www.dropbox.com/s/hmi7i6jcrkkwhxk/65700.h5 | tr -d '\r'
-wget https://www.dropbox.com/s/ssy2rb7cp5t3r1u/64753.h5 | tr -d '\r'
 python3 ensamble_test.py $1 $2
